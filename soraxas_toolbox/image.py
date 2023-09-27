eee5f4a3bbd0cf40b9c3c3d6ec818c619d34be7a
Tin Lai <oscar@tinyiu.com>
2023-09-27 12:10:44 +1000

    improve image displaying
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- soraxas_toolbox/image.py
+++ soraxas_toolbox/image.py
@@ -1,10 +1,11 @@
 import os
+import sys
 import math
 import warnings
 from shutil import which
 from subprocess import Popen, PIPE
 
-from typing import IO, Callable, Optional, Tuple, Union, List
+from typing import IO, Callable, Optional, Tuple, Union, List, Any
 
 import tqdm
 import PIL.Image
@@ -13,6 +14,44 @@ from . import easy_with_blocks, notebook
 
 if not which("timg"):
     raise ValueError("timg not installed")
+
+
+############################################################
+##             Turn any matplotlib plt to img             ##
+############################################################
+
+
+def plt_fig_to_nparray(fig):
+    """
+    Takes a matplotlib figure handle and converts it using
+    canvas and string-casts to a numpy array that can be
+    visualized in TensorBoard using the add_image function
+
+    Parameters:
+        fig (matplotlib.pyplot.fig): Matplotlib figure handle.
+    Return:
+        np.array with shape = (x,y,d) where d=3
+    """
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # remove white padding
+    fig.tight_layout()
+
+    # Draw figure on canvas
+    fig.canvas.draw()
+
+    # Convert the figure to numpy array, read the pixel values and reshape the array
+    img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
+    img = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+    # Normalize into 0-1 range for TensorBoard(X). Swap axes for newer versions where API expects colors in first dim
+    img = img / 255.0
+    # img = np.swapaxes(img, 0, 2) # if your TensorFlow + TensorBoard version are >= 1.8
+    plt.close(fig)
+    return img
+    # # Add figure in numpy "image" to TensorBoard writer
+    # writer.add_image('confusion_matrix', img, step)
 
 
 class TerminalImageViewer:
@@ -59,9 +98,6 @@ def __send_to_display(save_to_straem: Callable[[IO], None], pbar: tqdm.tqdm = No
                 pbar.write(out.decode())
 
 
-import sys
-
-
 def module_was_imported(module_name: str):
     return module_name in sys.modules
 
@@ -86,6 +122,98 @@ def _get_new_shape_maintain_ratio(
     return new_shape
 
 
+from abc import ABC
+
+
+class ArrayAutoFixer(ABC):
+    color_channel_idx: int
+    auto_fix_channel_idx: int
+    module: Any
+
+    @classmethod
+    def float_types(cls):
+        return cls.module.float16, cls.module.float32, cls.module.float64
+
+    @classmethod
+    def cls_var_setter(cls, module):
+        cls.module = module
+
+    @classmethod
+    def fix_channel(cls, x):
+        # if color channel is at the last dim, move it to the first dim (if not batched)
+        # _module is either torch or numpy
+
+        if len(x.shape) < 3:
+            return x
+
+        # color_channel_idx = 0 (the following can handle batched image as well)
+
+        # move color channel to corresponding idx
+        # if color channel is at the last dim, move it to the first dim (if not batched)
+        if x.shape[cls.color_channel_idx] not in (1, 3, 4) and x.shape[
+            cls.auto_fix_channel_idx
+        ] in (1, 3, 4):
+            x = cls.module.moveaxis(x, cls.auto_fix_channel_idx, cls.color_channel_idx)
+
+        return x
+
+    @classmethod
+    def fix_dtype(cls, x):
+        if x.dtype != cls.module.uint8 and x.dtype in cls.float_types():
+            x = (x * 255).astype(cls.module.uint8)
+        return x
+
+    @classmethod
+    def fix_float_range(cls, image, normalise: bool):
+        # works for either numpy or torch
+        _min = image.min()
+        _max = image.max()
+        _warning_msg = None
+        if normalise:
+            image = (image - _min) / (_max - _min)
+        elif _min < 0 or _max > 1:
+            if normalise is not False:
+                _warning_msg = (
+                    "Given image is of float-type, but its value is not between [0, 1]."
+                )
+                if _min >= 0 and _max <= 255:
+                    _warning_msg = f"{_warning_msg} Seems it's within [0, 255]. Gonna normalising it based on this."
+                image = image / 255
+
+        if _warning_msg is not None:
+            warnings.warn(_warning_msg, RuntimeWarning)
+
+        return image
+
+
+class NumpyArrayAutoFixer(ArrayAutoFixer):
+    # Width X Height X Color
+    color_channel_idx: int = -1
+    auto_fix_channel_idx: int = -3
+
+
+class TorchArrayAutoFixer(ArrayAutoFixer):
+    #  Batch X ... X Color X Width X Height
+    color_channel_idx: int = -3
+    auto_fix_channel_idx: int = -1
+
+    @classmethod
+    def infer_is_batch(cls, image):
+        # try to infer it
+        if len(image.shape) >= 4:
+            return True
+        elif len(image.shape) == 2:
+            return False
+        elif len(image.shape) == 3:
+            if image.shape[0] in (1, 3):
+                # probably is gray-scale or RGB image
+                return False
+            else:
+                # since it has non-standard number of channels, probably its a batched grey-scale image?
+                return True
+        raise NotImplementedError()
+
+
 def display(
     image,
     pbar: tqdm.tqdm = None,
@@ -93,11 +221,19 @@ def display(
     normalise: Optional[bool] = None,
     target_size: Tuple[int, int] = None,
     is_batched: Optional[bool] = None,
+    is_grayscale: Optional[bool] = None,
 ) -> None:
     if module_was_imported("numpy") and module_was_imported("PIL"):
         import numpy
 
         if isinstance(image, numpy.ndarray):
+            NumpyArrayAutoFixer.cls_var_setter(module=numpy)
+            if image.dtype in NumpyArrayAutoFixer.float_types():
+                # scale if necessary
+                image = NumpyArrayAutoFixer.fix_float_range(image, normalise=normalise)
+            # to uint8 if necessary
+            image = NumpyArrayAutoFixer.fix_channel(image)
+            image = NumpyArrayAutoFixer.fix_dtype(image)
             # convert to pillow image
             image = PIL.Image.fromarray(image)
 
@@ -112,6 +248,8 @@ def display(
         import torch
 
         if isinstance(image, torch.Tensor):
+            TorchArrayAutoFixer.cls_var_setter(module=torch)
+
             with torch.no_grad():
                 with easy_with_blocks.NoMissingModuleError(strong_warning=True):
                     import torchvision
@@ -130,34 +268,20 @@ def display(
                         # )
 
                     # for float or double
-                    _min = image.min()
-                    _max = image.max()
-                    _warning_msg = None
-                    if normalise:
-                        image = (image - _min) / (_max - _min)
-                    elif _min < 0 or _max > 1:
-                        if normalise is not False:
-                            _warning_msg = "Given image is of float-type, but its value is not between [0, 1]."
-                            if _min >= 0 and _max <= 255:
-                                _warning_msg = f"{_warning_msg} Seems it's within [0, 255]. Gonna normalising it based on this."
-                            image = image / 255
+                    image = TorchArrayAutoFixer.fix_float_range(
+                        image, normalise=normalise
+                    )
 
-                    if _warning_msg is not None:
-                        warnings.warn(_warning_msg, RuntimeWarning)
+                    if is_grayscale:
+                        if len(image.shape) > 2:
+                            is_batched = True
+                            image = image.reshape(-1, image.shape[-2], image.shape[-1])
+
+                    # potentially fix color channel index
+                    image = TorchArrayAutoFixer.fix_channel(image)
 
                     if is_batched is None:
-                        # try to infer it
-                        if len(image.shape) >= 4:
-                            is_batched = True
-                        elif len(image.shape) == 2:
-                            is_batched = False
-                        elif len(image.shape) == 3:
-                            if image.shape[0] in (1, 3):
-                                # probably is gray-scale or RGB image
-                                is_batched = False
-                            else:
-                                # since it has non-standard number of channels, probably its a batched grey-scale image?
-                                is_batched = True
+                        is_batched = TorchArrayAutoFixer.infer_is_batch(image)
 
                     if is_batched:
                         if len(image.shape) == 2:
@@ -166,7 +290,7 @@ def display(
                                 RuntimeWarning,
                             )
                         elif len(image.shape) == 3:
-                            # is grey scale
+                            # is gray scale
                             image = image.unsqueeze(1)
                     else:
                         if len(image.shape) == 2:
